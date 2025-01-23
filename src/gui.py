364f14923fbbdcb7e4debbab51b490cd0e18f7f3
Amine Moulay <amine.moulay@nucleon-security.com>
2025-01-23 12:11:05 +0100

    rename alert format

--- src/gui.py
+++ src/gui.py
@@ -238,7 +238,7 @@ class NetworkScanDetectorGUI(QWidget):
             return
 
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = f"alerts_{timestamp}.json"
+        filename = f"mouiraguard-nsd_alerts_{timestamp}.json"
         current_dir = os.getcwd()
 
         # Append 'logs' to the current directory to form the correct path
